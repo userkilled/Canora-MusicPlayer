d8d608faf4a6f06141341629dac9fd760ac1e732
Zoidberg <zoidCH@outlook.com>
2019-05-27 13:53:39 +0200

    public Theme Data

--- app/src/main/java/ch/swissproductions/canora/data_theme.java
+++ app/src/main/java/ch/swissproductions/canora/data_theme.java
@@ -1,8 +1,8 @@
 package ch.swissproductions.canora;
 
 public class data_theme {
-    String title;
-    Integer resID;
+    public String title;
+    public Integer resID;
 
     public data_theme(String t, int r) {
         title = t;
