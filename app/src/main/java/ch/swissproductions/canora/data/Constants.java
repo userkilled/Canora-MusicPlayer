7892fb269fa4b8b43c88f6cc18960deb15e44925
Zoidberg <zoidCH@outlook.com>
2019-05-28 18:47:36 +0200

    Tweaking Themes

--- app/src/main/java/ch/swissproductions/canora/data/Constants.java
+++ app/src/main/java/ch/swissproductions/canora/data/Constants.java
@@ -50,7 +50,7 @@ public final class Constants {
         Themes() {
             themes = new ArrayList<>();
             themes.add(new data_theme(Constants.THEME_BLUE, R.style.AppTheme_Blue));
-            themes.add(new data_theme(Constants.THEME_MINT, R.style.AppTheme_Mint));
+            themes.add(new data_theme(Constants.THEME_LIGHTBLUE, R.style.AppTheme_LightBlue));
             themes.add(new data_theme(Constants.THEME_DARK, R.style.AppTheme_Dark));
             themes.add(new data_theme(Constants.THEME_LIGHT, R.style.AppTheme_Light));
         }
@@ -61,7 +61,7 @@ public final class Constants {
     }
 
     public static final String THEME_BLUE = "Blue";
-    public static final String THEME_MINT = "Mint";
+    public static final String THEME_LIGHTBLUE = "Light Blue";
     public static final String THEME_DARK = "Dark";
     public static final String THEME_LIGHT = "Light";
 
